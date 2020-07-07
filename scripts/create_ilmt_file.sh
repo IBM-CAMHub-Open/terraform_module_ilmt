e20ec4d2b3f14bfeb63e0ecbcd9d40eea528632e
CAMHub PublishBot <octravis@us.ibm.com>
2020-07-07 16:21:13 +0000

    Automated CAMHub::publish on 2020-07-07 16:21:13 UTC

---
+++ scripts/create_ilmt_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ILMT_FILE_PATH=/etc/opt/CAM/swidtag
+ILMT_FILE_NAME=ibm.com_Terraform_Automation-2.0.0.swidtag
+ILMT_FILE=$ILMT_FILE_PATH/$ILMT_FILE_NAME
+
+if ! [ -f $ILMT_FILE ]; then
+  echo "$ILMT_FILE does not exist on this system, creating it now."
+  sudo mkdir -p $ILMT_FILE_PATH
+  
+  sudo cp /tmp/$ILMT_FILE_NAME $ILMT_FILE
+else
+  echo "$ILMT_FILE already exists on this system, do nothing."
+fi
